d29e00ce80a0f1de4e0aa33c6b4a885a16457e00
Brocolarbre <brocolarbre@gmail.com>
2025-11-17 09:54:07 +0100

    Fixed order

--- Windove/include/Windove/EventHandler.hpp
+++ Windove/include/Windove/EventHandler.hpp
@@ -15,12 +15,10 @@ namespace dove
 	public:
 		WINDOVE_API virtual ~EventHandler() = default;
 
-		WINDOVE_API virtual void onTextEntered(unsigned int keyCode) {};
 		WINDOVE_API virtual void onControllerConnected(Controller::Id controllerId) {};
 		WINDOVE_API virtual void onControllerDisconnected(Controller::Id controllerId) {};
 		WINDOVE_API virtual void onCursorEntered() {};
 		WINDOVE_API virtual void onCursorLeft() {};
-		WINDOVE_API virtual void onMouseMoved(unsigned int x, unsigned int y) {};
 		WINDOVE_API virtual void onDragAndDrop(const std::vector<std::string>& paths) {};
 		WINDOVE_API virtual void onFramebufferResized(unsigned int width, unsigned int height) {};
 		WINDOVE_API virtual void onKeyPressed(KeyEvent keyEvent) {};
@@ -28,9 +26,11 @@ namespace dove
 		WINDOVE_API virtual void onKeyRepeated(KeyEvent keyEvent) {};
 		WINDOVE_API virtual void onMonitorConnected(Monitor monitor) {};
 		WINDOVE_API virtual void onMonitorDisconnected(Monitor monitor) {};
+		WINDOVE_API virtual void onMouseMoved(unsigned int x, unsigned int y) {};
 		WINDOVE_API virtual void onMousePressed(MouseEvent mouseEvent) {};
 		WINDOVE_API virtual void onMouseReleased(MouseEvent mouseEvent) {};
 		WINDOVE_API virtual void onMouseScrolled(int horizontalScroll, int verticalScroll) {};
+		WINDOVE_API virtual void onTextEntered(unsigned int keyCode) {};
 		WINDOVE_API virtual void onWindowClosed() {};
 		WINDOVE_API virtual void onWindowContentScaled(float x, float y) {};
 		WINDOVE_API virtual void onWindowFocusGained() {};

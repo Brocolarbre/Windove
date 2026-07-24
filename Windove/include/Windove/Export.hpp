78afad9c8a45e18c5883b4e68fc8a01f88ad036a
Brocolarbre <brocolarbre@gmail.com>
2026-07-24 16:08:29 +0200

    Updated template

--- Windove/include/Windove/Export.hpp
+++ Windove/include/Windove/Export.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-#if defined(_WIN32)
-	#if defined(WINDOVE_STATIC)
-		#define WINDOVE_API
-	#elif defined(WINDOVE_BUILD)
-		#define WINDOVE_API __declspec(dllexport)
-	#else
-		#define WINDOVE_API __declspec(dllimport)
-	#endif
+#if defined(WINDOVE_STATIC)
+	#define WINDOVE_API
 #else
-	#define WINDOVE_API __attribute__((visibility("default")))
+	#if defined(_WIN32)
+		#if defined(WINDOVE_BUILD)
+			#define WINDOVE_API __declspec(dllexport)
+		#else
+			#define WINDOVE_API __declspec(dllimport)
+		#endif
+	#else
+		#define WINDOVE_API __attribute__((visibility("default")))
+	#endif
 #endif
